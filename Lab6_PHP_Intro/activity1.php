1cd98fb72722117e229299621bc7b5b1b44db5a7
1314863 <prinsobed@gmail.com>
2016-03-07 10:53:58 +0000

    PHP

--- Lab6_PHP_Intro/activity1.php
+++ Lab6_PHP_Intro/activity1.php
@@ -10,23 +10,57 @@
  * Time: 10:29
  */
 
-
+//SECTION 3
 //ACTIVITY 1 - SPECS, MUGS, AND SAUSAGE ROLLS
-$kofi_age = 17;
+//If Else
+$age = 17;
 
 $specs = 16;
 $mugs = 18;
 $sausage = 21;
 
-if($kofi_age >= $specs) {
-    print "Your are" . $kofi_age ."Years". ". You are eligible to buy Specs";
-}else if ($kofi_age >= $mugs ) {
-    print "Your are" . $kofi_age ."Years". ". You are eligible to buy Mugs";
-}else if ($kofi_age >= $sausage ) {
-    print "Your are" . $kofi_age . "Years" . ". You are eligible to Sausage";
+if($age >= $specs) {
+    print "Your are" . $age ."Years". ". You are eligible to buy Specs";
+}else if ($age >= $mugs ) {
+    print "Your are" . $age ."Years". ". You are eligible to buy Mugs";
+}else if ($age >= $sausage ) {
+    print "Your are" . $age . "Years" . ". You are eligible to Sausage";
 }
-else
-    print "You got to grow more Kofi.";
+else{
+    print "You got to grow more.";
+}
+
+//Switch
+$wantedgood = "mugs";
+
+switch($wantedgood){
+    case "specs":
+        echo "You have to be 16 to buy 'Specs'";
+        break;
+    case "mugs":
+        echo "You have to be 18 to buy 'Mugs'";
+        break;
+    case "sausage":
+        echo "You have to be 21 to buy 'Sausage'";
+        break;
+}
+
+//SECTION 4
+//ARRAYS
+$myArray = array("do", "re", "mi");  //Declaration of an array
+
+echo $myArray[0] // Output element at index 0
+
+    $myArray[1] = "la"; //Modifies content of element at index 1
+
+unset($myArray[2]); //Removes the array in index 2
+
+
+$provisionedActivities = array("Specs", "Drugs", "Rock and Roll");
+foreach($provisionedActivity as $x) {
+    print "<p>$x</p>";
+}
+
 
 
 <?>
--- Lab6_PHP_Intro/sec3.php
+++ Lab6_PHP_Intro/sec3.php
@@ -5,9 +5,36 @@
  * Date: 07/03/2016
  * Time: 09:48
  */
+
+// Simple code to printout
 echo "Hello World!, How've you been..";
 echo "Hello,"." "." World"."!";
 
+echo 5*7;
+
+//Variable Declaration & Concatenation
+$myname = "Mr. Frodo Baggins";
+$myage = 111;
+echo "My name is ".$myname." and i am ".$myage;
+
+// COMPARISONS
+$name = "Hargreaves";
+
+if($name=="Hargreaves") {
+    print"I know You";
+}
+else if($name == "Owen") {
+    print"Oh i see, You entered your first name.. Grr!";
+}
+else{
+    echo "Who are you?";
+}
 
 
-?>
+
+
+
+
+
+
+    ?>
