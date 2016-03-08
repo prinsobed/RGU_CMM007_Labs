499f6a07727e9a1e657d428645f593e258390589
1314863 <prinsobed@gmail.com>
2016-03-08 11:48:55 +0000

    Commit MC Files

--- Lab6_PHP_Intro/activity6.php
+++ Lab6_PHP_Intro/activity6.php
@@ -5,3 +5,24 @@
  * Date: 08/03/2016
  * Time: 09:32
  */
+
+//SECTION 6 - FUNCTIONS
+//String Functions
+
+$x = "xylemphloem";
+
+echo strlen($x);
+
+
+//Creation of an array
+$lotteryArray = array("Bilbo","Frodo","Gandalf","Samwise","Aragon","Logolas", "Gimli","Sauron","Elrond","Boromir");
+
+//Sort the array
+sort($lotteryArray);
+
+//Select the Winner
+$winner = (rand(0, count($lotteryArray))) - 1;
+
+
+
+?>
