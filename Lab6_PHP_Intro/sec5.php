b6a044914a482fc6614d8d7b5b34ea414870212f
Obed Kraine <prinsobed@gmail.com>
2016-03-08 03:15:06 +0000

    Update sec5.php

--- Lab6_PHP_Intro/sec5.php
+++ Lab6_PHP_Intro/sec5.php
@@ -5,30 +5,6 @@
  * Date: 07/03/2016
  * Time: 09:48
  */
- //SECTION 5
-        //FOR LOOPS
-        //Eg.
-        for ($i=1; $i<11; $i++){
-            echo "<p> Hello World !</p>";
-        }
-
-        //Activity 4 - Specs, Mugs, and Sausage IV
-        for ($x=1; $x<31; $x++){
-
-            if ($x % 2 == 0){
-                echo "<p>On the ".$x."th/nd of the Month, Specs are available</p>";
-            }
-            else if ($x % 3 == 0){
-                echo "<p>On the ".$x."rd of the Month, Mugs are available</p>";
-            }
-            else if ($x % 4 == 0){
-                echo "<p>On the ".$x."th/nd of the Month, Specs and Sausage are available</p>";
-            };
-
-        }
-
-
-//Mike Crabb's Version
 for ($i = 1; $i < 30; $i++)
 {
     $y = 0; //I'm using this as a marker to tell that at least one thing is being made
@@ -56,4 +32,4 @@ for ($i = 1; $i < 30; $i++)
     echo "</p>";
 }
 
-<?>
+?>
