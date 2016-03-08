a62530e11d466550cde70231b05653e156895742
1314863 <prinsobed@gmail.com>
2016-03-08 15:40:27 +0000

    Commit MC Files

--- Lab6_PHP_Intro/activity6.php
+++ Lab6_PHP_Intro/activity6.php
@@ -9,9 +9,13 @@
 //SECTION 6 - FUNCTIONS
 //String Functions
 
+//Lenght of String
 $x = "xylemphloem";
-
 echo strlen($x);
+
+
+
+
 
 
 //Creation of an array
@@ -23,6 +27,23 @@ sort($lotteryArray);
 //Select the Winner
 $winner = (rand(0, count($lotteryArray))) - 1;
 
+//Prints the winner
+echo "<p>The winner of all the specs is " . strtoupper($lotteryArray[$winner]) . "</p>";
+
+
+//Remove the person from the array
+unset($lotteryArray[$winner]);
+
+//Do the same again for mugs
+$winner = (rand(0,count($lotteryArray))) - 1;
+echo "<p>The winner of all the mugs is " . strtoupper($lotteryArray[$winner]) . "</p>";
+unset($lotteryArray[$winner]);
+
+
+//and the sausuage rolls
+$winner = (rand(0,count($lotteryArray))) - 1;
+echo "<p>The winner of all the sausage rolls is " . strtoupper($lotteryArray[$winner]) . "</p>";
+?>
 
 
 ?>
