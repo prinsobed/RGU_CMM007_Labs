0d18aee435c8bd623109d78c6c92d903cf43d57d
Obed Kraine <prinsobed@gmail.com>
2016-03-08 03:03:50 +0000

    Update sec5.php

--- Lab6_PHP_Intro/sec5.php
+++ Lab6_PHP_Intro/sec5.php
@@ -5,3 +5,24 @@
  * Date: 07/03/2016
  * Time: 09:48
  */
+ //SECTION 5
+        //FOR LOOPS
+        //Eg.
+        for ($i=1; $i<11; $i++){
+            echo "<p> Hello World !</p>";
+        }
+
+        //Activity 4 - Specs, Mugs, and Sausage IV
+        for ($x=1; $x<31; $x++){
+
+            if ($x % 2 == 0){
+                echo "On the ".$x."th/nd of the Month, Specs are available";
+            }
+            else if ($x % 3 == 0){
+                echo "On the ".$x."rd of the Month, Mugs are available";
+            }
+            else if ($x % 4 == 0){
+                echo "On the ".$x."th/nd of the Month, Specs and Sausage are available";
+            };
+
+        }
