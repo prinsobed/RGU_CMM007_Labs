30ec8e98f2992266136b2c744fb22656ce74616c
1314863 <prinsobed@gmail.com>
2016-03-14 18:27:43 +0000

    Lets go Home..!

---
+++ Lab7_Databases/home.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1314863
+ * Date: 14/03/2016
+ * Time: 17:59
+ */
+?>
