7b94da4b08c143f865085758dac6f65713b3f7ea
1314863 <prinsobed@gmail.com>
2016-03-15 09:45:32 +0000

    Comit Marvel

---
+++ Lab7b_MarvelWeb/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marvel Movies | Home</title>
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<main>
+    <header>
+        <img id="logo" src="assets/images/logo.jpg" alt="cdsm_logo">
+            <p>Marvel Cinematic Universe</p>
+                <nav>
+                    <a href="#">All Movies</a>
+                    <a href="#">Marvel Studio Movies</a>
+                    <a href="#">2010 Movies</a>
+                    <a href="#">X-Men Movies</a>
+                    <a href="#">Thor Movies</a>
+                    <a href="#">Contact Us</a>
+                </nav>
+
+        <div id="main_banner">
+            <img id="banner" src="assets/images/titles.jpg" alt="main banner" width="960px">
+        </div>
+    </header>
+
+    <div class="maincontent">
+
+    </div>
+
+</main>
+
+<div class="fixer"></div>
+<footer>
+    <p>Marvel Cinematic Universe © 2016 Obed Kraine LabWorks</p>
+</footer>
+
+</body>
+
+
+</html>
---
+++ Lab7b_MarvelWeb/login.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kraine
+ * Date: 3/14/2016
+ * Time: 5:53 AM
+ */
+include("dbConnect.php"); // Establish Connection with DB
+
+if(empty($_POST["username"])|| empty($_POST["password"])) {
+    echo "Both fields are required.";
+}else
+{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+}
+
+$sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
+
+$result=mysqli_query($db,$sql);
+
+if(mysqli_num_rows($result)==1)
+{
+    header("location: home.php");//Redirecting to another
+}else {
+    echo "Incorrect Username and Password.";
+}
+
+
+
+?>
