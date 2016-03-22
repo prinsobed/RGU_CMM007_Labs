7153eb78c80c754de28d34d944d35cb3386180ec
Obed Kraine <prinsobed@gmail.com>
2016-03-22 06:50:33 +0000

    PHP

--- Lab8b_Superheroes/results.php
+++ Lab8b_Superheroes/results.php
@@ -17,17 +17,6 @@
     <h1>Registration for Meta-Humans - Details Confirmation</h1>
 </header>
 <main>
-
-
-    <form action = "results.php" method = "post">
-        <input type = "text" placeholder = "firstName" name = "firstName">
-        <input type = "text" placeholder = "lastName" name = "lastName">
-        <input type="radio" name="gender" value="male" checked> Male<br>
-        <input type="radio" name="gender" value="female"> Female<br>
-        <input type="radio" name="gender" value="other"> Other
-        <input type = "text" placeholder = "mainPower" name = "mainPower">
-        <input type = "submit" value ='Go.. Go.. Goo..!'
-    </form>
     <?php
         $firstName = $_POST["firstName"];
         $lastName = $_POST["lastName"];
