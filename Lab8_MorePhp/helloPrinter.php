7cd6dadf6cdba96b84dd4dcd92b2cc4a0f0af6d0
Obed Kraine <prinsobed@gmail.com>
2016-03-22 05:35:58 +0000

    Printer

--- Lab8_MorePhp/helloPrinter.php
+++ Lab8_MorePhp/helloPrinter.php
@@ -9,27 +9,33 @@
     <h1>Using Urls to send information</h1>
 </header>
 <main>
-    <a href="helloSelector.php?userName=mike">HTML - Click Me</a>
 
-    <?php
-    $location = "helloSelector.php?username=mike";
-    header($location);
-    ?>
+<!--    <a href="helloSelector.php?userName=mike">HTML - Click Me</a>-->
+    <!---->
+    <!--    --><?php
+    //    $location = "helloSelector.php?username=mike";
+    //    header($location);
+    //    ?>
+    <!---->
+    <!--    --><?php
+    //    $username =$_GET["username"];
+    //    echo $username;
+    //    ?>
+    <!---->
+    <!--    --><?php
+    //    $username = $_GET["username"];
+    //    echo $username;
+    //
+    //    echo "<p>Hello".$username.", how are you today</p>";
+    //
+    //    echo "<p>Hello {$username}, how are you today</p>"
+    //    ?>
 
-    <?php
-    $username =$_GET["username"];
-    echo $username;
-    ?>
-
-    <?php
-    $username = $_GET["username"];
-    echo $username;
-
-    echo "<p>Hello".$username.", how are you today</p>";
-
-    echo "<p>Hello {$username}, how are you today</p>"
-    ?>
-
+    <form action = "helloSelector.php" method = "post">
+        <input type = "text" placeholder = "First Name" name = "firstName">
+        <input type = "text" placeholder = "Last Name" name = "lastName">
+        <input type = "submit" value ='Go.. Go.. Goo..!'
+    </form>
 
 </main>
 </body>
