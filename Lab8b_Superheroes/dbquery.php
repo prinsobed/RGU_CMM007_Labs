8b5a1c329bdb32aeb3d94fd64fd4c4529de8a102
Obed Kraine <prinsobed@gmail.com>
2016-03-22 09:55:25 +0000

    PHP db Connect

---
+++ Lab8b_Superheroes/dbquery.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The PHP Get Method</title>
+</head>
+<body>
+<header>
+    <h1>Registration for Meta-Humans</h1>
+</header>
+<main>
+
+
+    <form action = "results.php" method = "post">
+        <input type = "text" placeholder = "firstName" name = "firstName">
+        <input type = "text" placeholder = "lastName" name = "lastName">
+        <input type="radio" name="gender" value="male" checked> Male<br>
+        <input type="radio" name="gender" value="female"> Female<br>
+        <input type="radio" name="gender" value="other"> Other
+        <input type = "text" placeholder = "mainPower" name = "mainPower">
+        <input type = "submit" value ='Go.. Go.. Goo..!'
+    </form>
+
+
+
+
+
+</main>
+</body>
+</html>
+
--- Lab8b_Superheroes/results.php
+++ Lab8b_Superheroes/results.php
@@ -17,16 +17,42 @@
     <h1>Registration for Meta-Humans - Details Confirmation</h1>
 </header>
 <main>
-    <?php
-        $firstName = $_POST["firstName"];
-        $lastName = $_POST["lastName"];
-        $gender = &$_POST["gender"];
-        $mainPower = $_POST["mainPower"];
-        echo "Welcome ! " . $firstName." ".$lastName. "to our "." ".$gender. " team" ." on Director Coulson's Bus <br>";
+<!--    --><?php
+//        $firstName = $_POST["firstName"];
+//        $lastName = $_POST["lastName"];
+//        $gender = &$_POST["gender"];
+//        $mainPower = $_POST["mainPower"];
+//        echo "Welcome ! " . $firstName." ".$lastName. " to our ".$gender. " team" ." on Director Coulson's Bus <br>";
+//
+//    echo "Your ".$mainPower. " will be very useful";
+//    ?>
 
+    <?php
+
+    $servername = "ap-cdbr-azure-east-c.cloudapp.net";
+    $username = "bf3d941a653cf6";
+    $password = "68c64445";
+    $dbname = "veesoft";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!conn){
+        die("Connection Failed Buddie: ".mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO superheros('firstName', 'lastName', 'gender', 'mainPower')
+                VALUES ($firstName,$lastName,$gender,$mainPower)";
+
+    if (mysqli_query($conn,$sql)){
+        echo "New MetaHuman has joined S.H.I.E.L.D";
+    }else{
+        echo "Error: ".mysqli_connect_error($conn);
+
+    }
+
+    echo "Welcome ! " . $firstName." ".$lastName. " to our ".$gender. " team" ." on Director Coulson's Bus <br>";
     echo "Your ".$mainPower. " will be very useful";
     ?>
-
 
 </main>
 </body>
