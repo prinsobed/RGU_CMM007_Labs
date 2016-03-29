2ad9caa9175ce7292367cfd930d88f25cad2c689
Obed Kraine <prinsobed@gmail.com>
2016-03-29 07:21:43 +0100

    Team Edit

--- Lab8b_Superheroes/bus_team.php
+++ Lab8b_Superheroes/bus_team.php
@@ -10,27 +10,27 @@
 </header>
 <main>
     <?php
-        $firstName = $_POST["firstName"];
-        $lastName = $_POST["lastName"];
-        $gender = &$_POST["gender"];
-        $mainPower = $_POST["mainPower"];
-    ?>
+//        $firstName = $_POST["firstName"];
+//        $lastName = $_POST["lastName"];
+//        $gender = &$_POST["gender"];
+//        $mainPower = $_POST["mainPower"];
+//    ?>
+<!---->
+<!--    --><?php
+//
+//    $servername = "ap-cdbr-azure-east-c.cloudapp.net";
+//    $username = "bf3d941a653cf6";
+//    $password = "68c64445";
+//    $dbname = "veesoft";
+//
+//    $conn = mysqli_connect($servername, $username, $password, $dbname);
+//
+//    if (!$conn){
+//        die("Connection Failed Buddie: ".mysqli_connect_error());
+//    }
+//
 
-    <?php
-
-    $servername = "ap-cdbr-azure-east-c.cloudapp.net";
-    $username = "bf3d941a653cf6";
-    $password = "68c64445";
-    $dbname = "veesoft";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn){
-        die("Connection Failed Buddie: ".mysqli_connect_error());
-    }
-
-
-    //include("dbConnect.php"); // Establish Connection with DB
+    include("dbConnect.php"); // Establish Connection with DB
 
     $sql = "SELECT * FROM superheros";
     $myquery = mysqli_query($db,$sql);
