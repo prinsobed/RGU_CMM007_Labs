74ba544fdcf11748ae4ee5e355bc7f70c7bc27ab
Obed Kraine <prinsobed@gmail.com>
2016-03-29 19:20:18 +0100

    Superhero

--- Lab8b_Superheroes/index.php
+++ Lab8b_Superheroes/index.php
@@ -11,24 +11,29 @@
 <head>
     <meta charset="UTF-8">
     <title>S.H.I.E.L.D | Home</title>
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <header>
-    <h1>Welcome to the S.H.I.E.L.D New Avengers Programme</h1>
-</header>
-<main>
 
+    <img id="logo" src="assets/images/sh.png" alt="Logo">
+    <h1>Welcome to the S.H.I.E.L.D New Avengers Programme</h1>
 
     <nav>
-        <ul type="square" >
-            <a href = "form_superhero.php">Register as a Superhuman</a><br>
-            <a href = "form_battle.php">Add a Battle</a><br>
-            <br><br>
-            <a href = "report_superhero.php">View all Superheros</a><br>
-            <a href = "report_battle.php">View all Battles</a><br>
-            <a href = "report_superhero_select.php">Select Battles of Superheros</a><br>
+        <ul>
+            <li><a href = "form_superhero.php">Register as a Superhuman</a></li>
+            <li><a href = "form_battle.php">Add a Battle</a></li>
+            <li><a href = "report_superhero.php">View all Superheros</a></li>
+            <li><a href = "report_battle.php">View all Battles</a></li>
+            <li><a href = "report_superhero_select.php">Select Battles of Superheros</a></li>
         </ul>
     </nav>
+</header>
+<main>
+    <img id="banner" src="assets/images/joinavg.png" alt="main banner">
+
+
 
 </main>
 </body>
