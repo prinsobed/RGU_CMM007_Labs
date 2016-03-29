0c541eb1888bfaa931df08730dc78c3ac0728694
Obed Kraine <prinsobed@gmail.com>
2016-03-29 14:46:43 +0100

    Superhero

--- Lab8b_Superheroes/report_superhero_select.php
+++ Lab8b_Superheroes/report_superhero_select.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>S.H.I.E.L.D | Superheros </title>
+    <title>S.H.I.E.L.D | Superheros and their Battles </title>
 </head>
 <body>
 <header>
@@ -11,20 +11,26 @@
 <main>
     <?php
     include("dbConnect.php"); // Establish Connection with DB
-
-    $sql = "SELECT * FROM superheros";
-    $myquery = mysqli_query($db,$sql);
-
-    if ($myquery->num_rows > 0) {
-    echo "<table><tr><th>Name</th><th>Gender</th><th>Superpower</th></tr>";
-    }
-
-    while($row = $myquery->fetch_array()) {
-    echo "<tr><td>" . $row["firstName"] . " " . $row["lastName"] . "</td><td>" . $row["gender"] . "</td><td>" . $row["mainPower"] . "</td></tr>";
-    }
-
-    echo "</table>";
     ?>
+
+    <form action = "form_superhero_succ.php" method = "post">
+        <select name="Superheros">
+
+                <?php $superheros_query = "SELECT firstname, lastname FROM superherobattles";
+                      $results = $db->query($superheros_query);
+                while($row = $results->fetch_array())
+                {
+                    $fname = $row['firstName'];
+                    $lname = $row['lastName'];
+                    $gender = $row['gender'];
+                }?>
+            <option value = "<?php echo $fname." ".$lname?>">
+
+        </select>
+        <br><br>
+        <input type="submit"
+    </form>
+
 
     <p><a href = "form_superhero.php"> Register New Superhero </a><br></p>"
     <p><a href = "index.php"> Home Page </a><br></p>"
