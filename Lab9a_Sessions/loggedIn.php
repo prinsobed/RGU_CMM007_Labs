b8dd1dbac952444eaed75f8176c2573645a9961d
Obed Kraine <prinsobed@gmail.com>
2016-04-11 09:37:14 +0100

    LoggedIn

--- Lab9a_Sessions/loggedIn.php
+++ Lab9a_Sessions/loggedIn.php
@@ -5,11 +5,18 @@
  * Date: 4/11/2016
  * Time: 6:56 AM
  */
+session_start();
+$accesslevel = $_COOKIE['access_level_cookie'];
 
-$thisUser = $_POST["username"];
-$password = $_POST["password"];
+displayAccessLevelInformation($accesslevel);
 
-if($thisUser=="username" && $password="password")
+function displayAccessLevelInformation($accesslevel)
 {
-    setcookie()
+    if ($accesslevel == "standarduser"){
+        echo "<p style = \"background-color: lightgreen\"> You are currently logged in as a 'Standard User'</p>;
+    }
+    else if ($accesslevel == "root"){
+        echo "<p style = \"background - color: red\"> You are currently logged in as a 'Root User'</p>;
 }
+}
+?>
