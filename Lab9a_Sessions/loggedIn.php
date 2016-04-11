a5c560e2e6f10800cdc4a791171b9ef7b2ad0c08
Obed Kraine <prinsobed@gmail.com>
2016-04-11 09:48:40 +0100

    Lab9a

--- Lab9a_Sessions/loggedIn.php
+++ Lab9a_Sessions/loggedIn.php
@@ -13,10 +13,10 @@ displayAccessLevelInformation($accesslevel);
 function displayAccessLevelInformation($accesslevel)
 {
     if ($accesslevel == "standarduser"){
-        echo "<p style = \"background-color: lightgreen\"> You are currently logged in as a 'Standard User'</p>;
+        echo "<p style = \"background-color: lightgreen\"> You are currently logged in as a 'Standard User'</p>";
     }
     else if ($accesslevel == "root"){
-        echo "<p style = \"background - color: red\"> You are currently logged in as a 'Root User'</p>;
+        echo "<p style = \"background - color: red\"> You are currently logged in as a 'Root User'</p>";
 }
 }
 ?>
