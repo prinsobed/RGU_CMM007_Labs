802e47b7a1312e7320eb00460fa39907a4314600
Obed Kraine <prinsobed@gmail.com>
2016-04-24 21:37:45 +0100

    Mini-Mock Push

--- MiniMockLab1/action.php
+++ MiniMockLab1/action.php
@@ -6,13 +6,34 @@
  * Time: 9:12 PM
  */
 
-include("dbConnect.php"); // Establish Connection with DB
+//include("dbConnect.php"); // Establish Connection with DB
+//
 
-    $bugName=$_POST['bugname'];
-    $bugSummary=$_POST['bugsummary'];
-    $bugSummary=$_POST['bugcategory'];
+
+$servername = "ap-cdbr-azure-east-c.cloudapp.net";
+$username = "bf3d941a653cf6";
+$password = "68c64445";
+$dbname = "veesoft";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn){
+    die("Connection Failed Buddie: ".mysqli_connect_error());
+}
+
+$bugName=$_POST['bugname'];
+$bugSummary=$_POST['bugsummary'];
+$bugSummary=$_POST['bugcategory'];
+
 
     $sql = "INSERT INTO bugtracker(bugname, bugsummary, bugcategory)
                 VALUES ('$bugName','$bugSummary','$bugCategory')";
+
+    if (mysqli_query($conn,$sql)){
+        echo "New Bug Added";
+    }else{
+        echo "Error: ".mysqli_connect_error($conn);
+
+    }
 
 ?>
