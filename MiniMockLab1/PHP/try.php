335d86b64810c32e79658aa16e56a8bd3a72b090
prinsobed <prinsobed@gmail.com>
2016-04-29 17:27:53 +0100

    Just a Push..!

---
+++ MiniMockLab1/PHP/try.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug Tracker | Home</title>
+    <link type="text/css" href="../assets/css/styles.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<!--Start of Header-->
+<header>
+    <a href="index.php"><img src="../assets/images/logo.png" alt="Site Logo" id="logo"></a>
+    <section id="headText">
+        <h1>BugTracker</h1>
+        <h2>Keeping Track of all the pesky bugs</h2>
+    </section>
+</header>
+<!--End of Header-->
+
+<!--Start of Page Main-->
+<main>
+    <aside>
+        <nav>
+            <ul>
+                <li><a href="showbugs.php">All Bug Items</a> </li>
+                <li><a href="bugs.php">Android Bugs</a></li>
+                <li><a href="bugs.php">iOS Bugs</a> </li>
+                <li><a href="bugs.php">Windows Bugs</a></li>
+                <li><a href="addbugs.php">Insert Bug</a> </li>
+            </ul>
+        </nav>
+    </aside>
+    <section>
+        <div id="Main">
+            <?php
+            include("dbConnect.php"); // Establish Connection with DB
+
+            $sql = "SELECT * FROM bugtracker";
+            $myquery = mysqli_query($db,$sql);
+
+            if ($myquery->num_rows > 0) {
+                echo "<table><tr><th>Bug Name</th><th>Category</th><th>Summary</th></tr>";
+            }
+            while($row = $myquery->fetch_array()) {
+                //$movieID = $row['marvelMovieID']; $yearReleased = $row['yearReleased']; $titles =$row['title']; $prosStudio = $row['productionStudio']; $note = $row['notes'];
+
+                echo "<tr><td>" . $row["bugname"] . "</td><td>" . $row["bugcategory"] . "</td><td>" . $row["bugsummary"] . "</td></tr>";
+            }
+            echo "</table>";
+            ?>
+        </div>
+    </section>
+</main>
+<!--End of Page Main-->
+
+<!--Start of Footer-->
+<footer>
+    <p>Designed by Obed Kraine Boachie, 2016.</p>
+</footer>
+<!--End of Footer-->
+</body>
+</html>
