33d75bf7c297b087c0136e49846e92ba700bb31b
prinsobed <prinsobed@gmail.com>
2016-08-13 14:34:03 +0100

    All Testors

--- PP_New/admin/delete.php
+++ PP_New/admin/delete.php
@@ -101,60 +101,8 @@ session_start();
   							<article>
         <div id="add_user">
             
-            <?php
-/*
-Allows the user to both create new records and edit existing records
-*/
-
-// connect to the database
-$servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
-$username="bed8c15b456030"; // Mysql username
-$password="58380471"; // Mysql password
-$dbname="db_prodpredict"; // Database name
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// creates the new/edit record form
-// since this form is used multiple times in this file, I have made it a function that is easily reusable
-function renderForm($first = '', $last ='',$company ='', $email ='', $password ='', $user_type ='', $error = '', $id = '')
-
-{ ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-<title>
-<?php if ($id != '') { echo "Edit Record"; } else { echo "New Record"; } ?>
-</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-</head>
-<body>
-<h1><?php if ($id != '') { echo "Edit Record"; } else { echo "New Record"; } ?></h1>
-<?php if ($error != '') {
-echo "<div style='padding:4px; border:1px solid red; color:red'>" . $error
-. "</div>";
-} ?>
-
-<form action="" method="post">
-<div>
-<?php if ($id != '') { ?>
-<input type="hidden" name="id" value="<?php echo $id; ?>" />
-<p>ID: <?php echo $id; ?></p>
-<?php } ?>
-
-<strong>First Name: *</strong> <input type="text" name="firstname"
-value="<?php echo $first; ?>"/><br/>
-<strong>Last Name: *</strong> <input type="text" name="lastname"
-value="<?php echo $last; ?>"/>
-<p>* required</p>
-<input type="submit" name="submit" value="Submit" />
-</div>
-</form>
-
+            
+            <!-- Code Here -->
         </div>
     </article>
                             
--- PP_New/admin/view_users.php
+++ PP_New/admin/view_users.php
@@ -25,6 +25,8 @@ session_start();
     <script src="../assets/js/npm.js"></script>
 
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="jquery.tabledit.js"></script>
 
 </head>
 <!-- End of Head -->
@@ -106,62 +108,122 @@ session_start();
   							<article>
         <div id="add_user">
 
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Password</th>
-                    <th>Type</th>
-                </tr>
-                </thead>
-            <?php
-            include "../includes/ajax.php";
 
-            $servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
-            $username="bed8c15b456030"; // Mysql username
-            $password="58380471"; // Mysql password
-            $dbname="db_prodpredict"; // Database name
 
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
+
+<?php
+
+$servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
+$username="bed8c15b456030"; // Mysql username
+$password="58380471"; // Mysql password
+$dbname="db_prodpredict"; // Database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+// number of results to show per page
+$per_page = 3;
+
+// figure out the total pages in the database
+if ($result = $mysqli->query("SELECT * FROM users ORDER BY id"))
+{
+    if ($result->num_rows != 0)
+    {
+        $total_results = $result->num_rows;
+// ceil() returns the next highest integer value by rounding up value if necessary
+        $total_pages = ceil($total_results / $per_page);
+
+// check if the 'page' variable is set in the URL (ex: view-paginated.php?page=1)
+        if (isset($_GET['page']) && is_numeric($_GET['page']))
+        {
+            $show_page = $_GET['page'];
+
+// make sure the $show_page value is valid
+            if ($show_page > 0 && $show_page <= $total_pages)
+            {
+                $start = ($show_page -1) * $per_page;
+                $end = $start + $per_page;
             }
-
-
-            $sql = "SELECT id, firstname, lastname, company, email, password, user_type FROM users";
-
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    echo '
-
-                <tbody>
-                <tr class="odd">
-                    <td id="id" contenteditable="false">' .$row["id"].'</td>
-                    <td id="f_name" contenteditable="true">' .$row["firstname"].'</td>
-                    <td id="l_name" contenteditable="true">' .$row["lastname"].'</td>
-                    <td id="company" contenteditable="true">' .$row["company"].'</td>
-                    <td id="email" contenteditable="true">' .$row["email"].'</td>
-                    <td id="password" contenteditable="true">' .$row["password"].'</td>
-                    <td id="u_type" contenteditable="true">' .$row["user_type"].'</td>
-                </tr>
-
-                </tbody>
-            ';
-                }
-            } else {
-                echo "0 results";
+            else
+            {
+// error - show first set of results
+                $start = 0;
+                $end = $per_page;
             }
-            $conn->close();
-            ?>
+        }
+        else
+        {
+// if page isn't set, show first set of results
+            $start = 0;
+            $end = $per_page;
+        }
+
+// display pagination
+        echo "<p><a href='admin_view_users.php'>View All</a> | <b>View Page:</b> ";
+        for ($i = 1; $i <= $total_pages; $i++)
+        {
+            if (isset($_GET['page']) && $_GET['page'] == $i)
+            {
+                echo $i . " ";
+            }
+            else
+            {
+                echo "<a href='admin_view_users_pag.php?page=$i'>$i</a> ";
+            }
+        }
+        echo "</p>";
+
+// display data in table
+        echo "<table class='table'><thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Company</th><th>Email</th><th>Password</th><th>Type</th></tr></thead>";
+
+
+// loop through results of database query, displaying them in the table
+        for ($i = $start; $i < $end; $i++)
+        {
+// make sure that PHP doesn't try to show results that don't exist
+            if ($i == $total_results) { break; }
+
+// find specific row
+            $result->data_seek($i);
+            $row = $result->fetch_row();
+
+// echo out the contents of each row into a table
+            echo "<tr>";
+            echo '<td>' . $row[0] . '</td>';
+            echo '<td>' . $row[1] . '</td>';
+            echo '<td>' . $row[2] . '</td>';
+            echo '<td>' . $row[3] . '</td>';
+            echo '<td>' . $row[4] . '</td>';
+            echo '<td>' . $row[5] . '</td>';
+            echo '<td>' . $row[6] . '</td>';
+            echo '<td><a href="admin_edit_user.php?id=' . $row[0] . '">Edit</a></td>';
+            echo '<td><a href="delete.php?id=' . $row[0] . '">Delete</a></td>';
+            echo "</tr>";
+        }
+
+// close table>
+        echo "</table>";
+    }
+    else
+    {
+        echo "No results to display!";
+    }
+}
+// error with the query
+else
+{
+    echo "Error: " . $mysqli->error;
+}
+
+// close database connection
+$mysqli->close();
+
+?>
             </table>
             <!-- Code Here -->
         </div>
