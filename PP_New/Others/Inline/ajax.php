434cc08d450d5f4a56696c0ad3bc9d61814240cf
prinsobed <prinsobed@gmail.com>
2016-08-13 12:00:17 +0100

    All Testors

--- PP_New/Others/Inline/ajax.php
+++ PP_New/Others/Inline/ajax.php
@@ -2,7 +2,18 @@
 if(!empty($_POST))
 {
 	//database settings
-	include "config.php";
+	$servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
+	$username="bed8c15b456030"; // Mysql username
+	$password="58380471"; // Mysql password
+	$dbname="db_prodpredict"; // Database name
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
 	foreach($_POST as $field_name => $val)
 	{
 		//clean post values
